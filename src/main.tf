3036b9edab0ab47dafb2b3bfcffb1e926d3b6ac6
Fabricio Veronez <fabricioveronez@gmail.com>
2024-05-07 22:40:09 -0300

    Alteração na quantidade de nós

--- src/main.tf
+++ src/main.tf
@@ -41,9 +41,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size       = 2
-      max_size       = 2
-      desired_size   = 2
+      min_size       = 4
+      max_size       = 4
+      desired_size   = 4
       instance_types = var.aws_eks_managed_node_groups_instance_types
       tags           = var.aws_project_tags
     }
